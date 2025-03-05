fc685af0edc91528e6fa21b7db16ab0df8d203d0
juan pablo <juanpabloosorio2406@gmail.com>
2025-03-05 14:17:16 -0500

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_26_004457) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_26_211509) do
+  create_table "bocks", force: :cascade do |t|
+    t.string "nombre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "cars", force: :cascade do |t|
     t.string "modelo"
     t.string "placa"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "personas", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tarjetas", force: :cascade do |t|
+    t.integer "code"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- test/models/juego_test.rb
+++ test/models/juego_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class CarsControllerTest < ActionDispatch::IntegrationTest
+class JuegoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
